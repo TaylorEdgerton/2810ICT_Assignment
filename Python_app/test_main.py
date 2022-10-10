8a308cfe33bef28e57a8c5e88683ad81a625d4c9
Taylor <taylor@edgerton.com.au>
2022-10-10 19:07:50 +1000

    ""

---
+++ Python_app/test_main.py
@@ -0,0 +1,53 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
